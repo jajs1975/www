5c5b18f5bac5f3d76364d282ae53c7dc567734cd
jajs1975 <george190475@hotmail.com>
2014-12-05 17:16:12 -0600

    back button
    back button

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
     /* --------------------------------- Event Registration -------------------------------- */
     
     document.addEventListener('deviceready', function () {	
-        alert("entra a onDeviceReady....");
+        alert("entra a onDeviceReady....xxxx");
         document.addEventListener("backbutton", onBackKeyDown, false);
         document.addEventListener("backbutton", this.onBackKeyDown, false);
         document.addEventListener("backbutton", function (e) {
