ace43228559800a2db5c56e67e107c0d105caace
jajs1975 <george190475@hotmail.com>
2015-02-18 17:15:00 -0600

    spribo18V10
    spribo18V10

--- js/vp_display.js
+++ js/vp_display.js
@@ -13,12 +13,24 @@ $.ajax({
           "dataType": "json"
     }).done(function(response){
 	var  value = "";
+	alert('respuesta: ' + JSON.stringify(response));
 	 if(response.Values){
 		for(var i=0; i<response.Values.length ; i++){
 			value+= response.Values[i];
 			value+= "<br>";
 		}
+	} else {
+		alert('response value es false');
 	}
+	
+	/*var z = JSON.parse(response);
+	for(var i in z) {
+		for(var j in x[i]) {
+			console.log(j, '-->' ,z[i][j]);
+		}
+	}*/
 		$('#propValue').html(value);
+		alert('Id del propValue: ' + document.getElementById('propValue').innerHTML + 'Id del subtitle: ' + document.getElementById('subtitle').innerHTML);
         $('#subtitle').html(response.Name);
+		//alert();
 }); 
